e42db543bc3bf0a3bf0e1c370f8ed376f8561b23
seyoung97 <108709932+seyoung97@users.noreply.github.com>
2022-10-12 10:53:13 +0900

    [4팀 정세영] Feature: Header UI (#1)
    * [Rename]: Header 파일 추가, Header, Logo, RecordButton, Duration jsx 파일 생성
    
    * [Design]: Header UI
    
    * [Add]: theme.jsx 에 main-color 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import GlobalStyle from './GrobalStyle';
-import Play from './page/Play';
+import Play from './components/Play/Play';
 import PlayList from './page/PlayList';
 import Record from './page/Record';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Routes>
         <Route path='/' element={<Play />} />
         <Route path='/record' element={<Record />} />
---
+++ src/components/Play/Play.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Duration from './Duration';
+
+const Play = () => {
+  return (
+    <section>
+      <Duration />
+    </section>
+  );
+};
+
+export default Play;
