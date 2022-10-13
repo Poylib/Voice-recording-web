a95b77655765fe27de07bd77f032769fec8a7445
Joey <seyoung1st@gmail.com>
2022-10-13 19:26:20 +0900

    [Design]재생 리스트 ui

--- src/components/Play/Download.jsx
+++ src/components/Play/Download.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { BiDownload } from 'react-icons/bi';
+
 const Download = () => {
   // useEffect(() => {
   //   if (track) {
@@ -10,9 +12,21 @@ const Download = () => {
   // }, [track, audioType]);
 
   let url = 'https://www.bensound.com/bensound-music/bensound-love.mp3';
-  return <DownloadButton href={url}>Download</DownloadButton>;
+  return (
+    <DownloadButton href={url}>
+      <BiDownload className='download-btn' />
+    </DownloadButton>
+  );
 };
 
-let DownloadButton = styled.a``;
+let DownloadButton = styled.a`
+  width: 40px;
+  height: 40px;
+  .download-btn {
+    color: #aaa;
+    width: 40px;
+    height: 40px;
+  }
+`;
 
 export default Download;
